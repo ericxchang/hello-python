425940f61af417f309e570ea2d79dde231cdc69f
Eric Chang <echang@pebblepost.com>
2019-09-14 09:32:02 -0400

    test module reference

---
+++ __init__.py
@@ -0,0 +1,2 @@
+#list sub modules to be included when import *
+_all_ = ["repo", "util"]
---
+++ repo/userDefiniton.py
@@ -0,0 +1,22 @@
+from .util import database as db
+
+print("__name__ value: ", __name__)
+def main():
+    print("Query uder_definition ...\n")
+    myDB = db.Database(
+        user="dashboard",
+        password="dashboard",
+        host="localhost",
+        port="5432",
+        database="dashboard"
+    )
+
+    result = myDB.query("select * from operations.user_definition")
+
+    for row in result:
+        print(row, "\n")
+
+    myDB.close()
+
+if __name__ == '__main__':
+    main()
